fc775ebd0d9a9085d113f77b2abe2665b9247caa
erozonachi <erozonachi.james@gmail.com>
2019-06-18 15:50:54 +0100

    Create and use handle-click() in todo-item component

--- todo/src/components/todos/TodoItem.js
+++ todo/src/components/todos/TodoItem.js
@@ -2,8 +2,15 @@ import React from 'react'
 import { List } from 'semantic-ui-react'
 
 const TodoItem = props => {
+  const handleClick = () => {
+    props.toggleCompleted({
+      id: props.todo.id,
+      completed: !props.todo.completed,
+    });
+  }
+
   return (
-    <List.Item>
+    <List.Item onClick={handleClick}>
       <List.Content>
         {props.todo.value}
       </List.Content>
