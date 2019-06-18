5b67ad057ee760e4304227960e94eb4f89385d5e
erozonachi <erozonachi.james@gmail.com>
2019-06-18 13:26:52 +0100

    Export todos reducer function

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,6 +1,7 @@
 import { CREATE_NEW_TODO, TOGGLE_TODO_COMPLETED, DELETE_TODO } from `../actions/action`;
 
-const todosReducer = (state = [], action) => {
+
+export const todosReducer = (state = [], action) => {
   switch (action.type) {
     case CREATE_NEW_TODO:
       return state.concat(action.payload);
