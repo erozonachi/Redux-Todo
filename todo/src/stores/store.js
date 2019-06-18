64cfd0aa9889dc9f07a9c9ce7f704271bed420d8
erozonachi <erozonachi.james@gmail.com>
2019-06-18 14:50:11 +0100

    Pass redux-devtools-extension to create-store()

--- todo/src/stores/store.js
+++ todo/src/stores/store.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
 import { todosReducer as todos } from '../reducers/reducer';
 
-export const store = createStore(todos);
+export const store = createStore(todos,  
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
