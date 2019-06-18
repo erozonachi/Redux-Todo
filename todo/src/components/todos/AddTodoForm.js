92dcdbf205f8e819bd4b56da1bd229420254ceff
erozonachi <erozonachi.james@gmail.com>
2019-06-18 15:22:06 +0100

    Create and use handle-submit() in add-todo-form

--- todo/src/components/todos/AddTodoForm.js
+++ todo/src/components/todos/AddTodoForm.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { Form } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { addTodo } from '../../actions/creators';
+import uuid from 'uuid';
 
 const AddTodoForm = props => {
   const taskInput = React.createRef();
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    if (taskInput.current.value.trim() !== '') {
+      props.addTodo(
+        {
+          id: uuid(),
+          value: taskInput.current.value,
+          completed: false,
+        }
+      );
+      taskInput.current.value = '';
+    }
+  }
   return (
-    <Form onSubmit={() => {}}>
+    <Form onSubmit={handleSubmit}>
       <Form.Group>
         <input placeholder='Enter Task' ref={taskInput} />
         <Form.Button type='submit' content='Add Task' />
