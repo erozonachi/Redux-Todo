61d14c24f0fbd6d6b882e5f8a2be58ede11f3ebd
erozonachi <erozonachi.james@gmail.com>
2019-06-18 16:23:28 +0100

    import and use todo-list in app component

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AddTodoForm from './components/todos/AddTodoForm';
+import TodoList from './components/todos/TodoList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <TodoList />
       <AddTodoForm />
     </div>
   );
