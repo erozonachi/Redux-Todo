a336574895d85eecc2d9e5b8146bb4f8e2c66fc7
erozonachi <erozonachi.james@gmail.com>
2019-06-18 17:16:27 +0100

    Import and use delete-todo in todo-list component

--- todo/src/components/todos/TodoList.js
+++ todo/src/components/todos/TodoList.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { List, Segment } from 'semantic-ui-react'
 import TodoItem from './TodoItem';
 import { connect } from 'react-redux';
-import { toggleCompleted } from '../../actions/creators';
+import { toggleCompleted, deleteTodo } from '../../actions/creators';
 
 const TodoList = props => {
   const todoList = props.todos.map(todo => <TodoItem 
     key={todo.id} 
     todo={todo}
-    toggleCompleted={props.toggleCompleted}  />)
+    toggleCompleted={props.toggleCompleted}
+    deleteTodo={props.deleteTodo}  />)
   return (
     <Segment inverted>
       <List divided inverted relaxed>
@@ -20,4 +21,4 @@ const TodoList = props => {
 
 const mapStateToProps = state => ({todos: state});
 
-export default connect(mapStateToProps, {toggleCompleted})(TodoList);
+export default connect(mapStateToProps, {toggleCompleted, deleteTodo})(TodoList);
