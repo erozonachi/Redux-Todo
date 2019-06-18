267b4f15458f8cd3199ce9bc6f96b3b5c1cfe3b0
erozonachi <erozonachi.james@gmail.com>
2019-06-18 13:55:21 +0100

    Create and export add-todo-form component

---
+++ todo/src/components/todos/AddTodoForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Form from 'semantic-ui-react';
+
+const AddTodoForm = props => {
+  const taskInput = React.createRef();
+  return (
+    <Form onSubmit={() => {}}>
+      <Form.Group>
+        <Form.Input placeholder='Enter Task' ref={taskInput} />
+        <Form.Button type='submit' content='Add Task' />
+      </Form.Group>
+    </Form>
+  );
+}
+
+export default AddTodoForm;
